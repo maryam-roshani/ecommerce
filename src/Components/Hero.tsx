b3b21bc6bb5db08d72858647c0cc07b095b3e851
Maryam Roshani <maryamroshani777@gmail.com>
2025-09-25 16:25:17 +0330

    edit hero component

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -51,7 +51,8 @@ const NextArrow = (props: any) => {
 };
 
 const Hero: React.FC = () => {
-
+  const [menu, setMenu] = useState("shop");
+  
   const slides = [
     { id: 1, path: "/men", img: men, label: "men" },
     { id: 2, path: "/women", img: women, label: "women" },
@@ -92,6 +93,9 @@ const Hero: React.FC = () => {
           </Box>
         ))}
       </Slider>
+      <Box sx={{ mt: 2, textAlign: "center" }}>
+        <p>Selected category: {menu}</p>
+      </Box>
     </Box>
   );
 };
